a987279fd752354175f90ef9d39188aeac8ef69a
apenkov <organaizer1212@gmail.com>
2020-12-08 11:42:49 +0300

    fix react route

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import {Route, BrowserRouter, Switch, Link} from "react-router-dom";
 import Home from "../Pages/Home";
 import Contacts from "../Pages/Contacts";
 import Blog from "../Pages/Blog";
@@ -8,25 +8,25 @@ export default class Header extends Component {
   render() {
     return (
       <>
-        <Router>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
           <div className={'header'}>
             <div className={'innerHeader'}>
               <div className={'logoContainer'}>
                 <h1>MY<span>BLOG</span></h1>
               </div>
               <ul className={'navigation'}>
-                <a href={'/'}><li>Home</li></a>
-                <a href={'/blog'}><li>Blog</li></a>
-                <a href={'/contacts'}><li>Contacts</li></a>
+                <Link to={process.env.PUBLIC_URL + '/'}><li>Home</li></Link>
+                <Link to={process.env.PUBLIC_URL +'/blog'}><li>Blog</li></Link>
+                <Link to={process.env.PUBLIC_URL +'/contacts'}><li>Contacts</li></Link>
               </ul>
             </div>
           </div>
-            <Switch>
-              <Route exact path={"/"} component={Home}/>
-              <Route exact path={"/contacts"} component={Contacts}/>
-              <Route exact path={"/blog"} component={Blog}/>
-            </Switch>
-        </Router>
+          <Switch>
+              <Route exact path={process.env.PUBLIC_URL +"/"} component={Home}/>
+              <Route  path={process.env.PUBLIC_URL +"/contacts"} component={Contacts}/>
+              <Route  path={process.env.PUBLIC_URL +"/blog"} component={Blog}/>
+          </Switch>
+        </BrowserRouter>
       </>
     )
   }
